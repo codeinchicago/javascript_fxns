daaa4a0e26f2eac93e98c2218868b162527515c3
codeinchicago <vancewcole@gmail.com>
2022-06-15 08:33:53 -0500

    Problem 2 done.

--- homework.js
+++ homework.js
@@ -9,18 +9,9 @@
 
 
 function toCelcius(num){
-    // loop through the array via index (i)
     var cel = (num-32) * 5/9;
     return cel
 }
-
-
-
-
-
-
-
-
 
 /*
     Exercise 2: Create a class for a User that sets username and password property
@@ -29,14 +20,27 @@ function toCelcius(num){
     Create an instance of the new user and then change their password using the method.
 */
 
+class User{
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+
+    }
+    change(oldpassword, newpassword){
+        if(oldpassword == this.password){
+            this.password = newpassword
+        }
+        else{
+            console.log('Wrong password.')
+        }
+    }
+}
 
 
-
-
-
-
-
-
+let me = new User('me','guess');
+me.change('sdfadsdfsafsd', 'sdffdssfdsdf')
+me.change('guess', 'now')
+console.log(me);
 
 /*
     Exercise 3: Write a function that takes in an array of integers and returns
@@ -47,8 +51,80 @@ function toCelcius(num){
     Ex 3. squareNegatives([2, 4, 6, 8, 10]) // []
 */
 
+const one = [-10, -3, 4, -2, 8, 9, -3]
+
+function isNegative(number){
+    if(number < 0){
+        return number
+    }
+}
+
+function squareNegatives(arr){
+    const squared = []
+    squared.push((arr.forEach(isNegative)))
+    return squared
+}
+
+for (let i = 0; i<(exerciseOneArr.length -1); i++){
+    if(exerciseOneArr[i] + 1 !== exerciseOneArr[i+1]) {
+    console.log(i+1)}
+  }
+
+
+names.forEach(callBack);
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Kiwi");
+
+// Array.prototype.filter()
+let cities = ['Chicago', 'Boston', 'Chattanooga', 'Baton Rouge', 'Champaign', 'Denver'];
+console.log(cities);
+
+function startsWithC(cityName){
+    return cityName[0].toUpperCase() === 'C'
+}
+
+let cNameCitiesMap = cities.map(startsWithC);
+console.log(cNameCitiesMap);
+let cNameCitiesFilter = cities.filter(startsWithC);
+console.log(cNameCitiesFilter);
 
 
 
 
+function isNegative(number){
+    if(number < 0){
+        return number
+    }
+}
 
+class Animal{
+    constructor(name, energy=10){
+        this.name = name;
+        this.energy = energy;
+    }
+
+    eat(amount){
+        this.energy += amount;
+        console.log(`${this.name} is eating and energy is now ${this.energy}`)
+    }
+
+    sleep(amount){
+        this.energy += amount;
+        console.log(`${this.name} is sleeping and energy is now ${this.energy}`)
+    }
+
+    play(amount){
+        this.energy -= amount;
+        console.log(`${this.name} is playing and energy is now ${this.energy}`)
+    }
+}
+
+let buddy = new Animal('Buddy', 10);
+buddy.eat(10);
+buddy.play(5);
+console.log(buddy);
+
+let leo = new Animal('Leo', 20);
+leo.eat(5);
+leo.play(15);
+console.log(leo);
